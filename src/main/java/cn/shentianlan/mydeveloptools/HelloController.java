fed7a827542495bde621d4725b9b65b68e305fc0
shentianlan <2869156923@qq.com>
2023-08-29 01:10:22 +0800

    初始化

---
+++ src/main/java/cn/shentianlan/mydeveloptools/HelloController.java
@@ -0,0 +1,14 @@
+package cn.shentianlan.mydeveloptools;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
